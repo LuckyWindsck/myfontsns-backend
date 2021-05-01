e189c9740b403ba53dbd3c3a99d5fc42962b9b4d
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-02 01:07:26 +0900

    test: add test for root path

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
@@ -13,8 +14,24 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.ts',
+        ],
+      },
+    },
+  },
   rules: {
+    'import/extensions': [
+      'error',
+      {
+        ts: 'ignorePackages',
+      },
+    ],
     'no-console': [
       'warn',
       {
