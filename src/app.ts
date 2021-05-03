576f27bdc59eed082b6695a4500dccdb7494c9ea
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-03 15:55:25 +0900

    feat: implement user creation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
-import { sequelize } from '../models';
+import { sequelize, User } from '../models';
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/users', async (req, res) => {
+  const user = await User.create(req.body);
+
+  res.status(201).send(user);
 });
 
 export { app, sequelize };
