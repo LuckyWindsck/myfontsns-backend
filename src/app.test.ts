d57d64f3298e2852400ae70913668eee529d4f42
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-03 15:17:57 +0900

    test: should be able to create a user

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,10 +1,41 @@
 import request from 'supertest';
-import { app } from './app';
+import faker from 'faker';
+
+import { User } from '../models';
+import { app, sequelize } from './app';
 
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
     const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
+  });
+});
+
+describe('Test /users', () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.sync({ force: true });
+    await sequelize.close();
+  });
+
+  test('It should be able to create a user', async () => {
+    const fakeUser = {
+      name: faker.random.alphaNumeric(15),
+      screenName: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    const response = await request(app).post('/users').send(fakeUser);
+
+    const user = await User.findOne({ where: fakeUser });
+
+    expect(user).not.toBeNull();
+    expect(response.status).toBe(201);
+    expect(response.body).not.toBe('');
   });
 });
