930c52906f1f727dc8618fbc5a296c8781d9dbc6
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-04 10:05:15 +0900

    test: implement tests for User & fix tests
    1. Implement tests for the following route:
      - GET '/users'
      - POST '/users'
      - GET '/users/:id'
      - PUT '/users/:id'
      - DELETE '/users/:id'
    
    2. Seeding user data before each test
    
    3. It seems like supertest (superagent) library will parse resonse.body,
       so I test response.body as a user object instead of string.

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -5,16 +5,25 @@ import { StatusCodes } from 'http-status-codes';
 import { User } from '../src/models';
 import { app, sequelize } from '../src/app';
 
-const makeFakeUser = () => ({
+const buildFakeUser = () => ({
   name: faker.random.alphaNumeric(15),
   screenName: faker.internet.userName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
 });
 
+// Normalize because createdAt & updatedAt are Date in user but string in response.body
+const normalize = (obj) => JSON.parse(JSON.stringify(obj));
+
 describe('Test /users', () => {
   beforeAll(async () => {
     await sequelize.sync({ force: true });
+
+    // Seeding
+    const fakeUserCount = 25;
+    const fakeUsers = Array(fakeUserCount).fill(null).map(buildFakeUser);
+
+    await User.bulkCreate(fakeUsers);
   });
 
   afterAll(async () => {
@@ -22,29 +31,78 @@ describe('Test /users', () => {
     await sequelize.close();
   });
 
-  test('It should be able to get all users', async () => {
+  test('It should display all users', async () => {
+    const { count: userCount, rows: users } = await User.findAndCountAll();
+    const { status, body: responseUsers } = await request(app).get('/users');
 
+    /* Response */
+    // Should have status 200
+    expect(status).toBe(StatusCodes.OK);
+    // Should respond with the amount of users same as the amount of users in database
+    expect(responseUsers.length).toBe(userCount);
+    // Should respond with all users in database
+    expect(responseUsers).toMatchObject(normalize(users));
   });
 
-  test('It should be able to create a user', async () => {
-    const fakeUser = makeFakeUser();
-    const response = await request(app).post('/users').send(fakeUser);
+  test('It should create a user', async () => {
+    const fakeUser = buildFakeUser();
+    const { status, body: responseUser } = await request(app).post('/users').send(fakeUser);
     const user = await User.findOne({ where: fakeUser });
 
+    /* Database */
+    // Should find user in database after POST request
     expect(user).not.toBeNull();
-    expect(response.status).toBe(StatusCodes.CREATED);
-    expect(response.body).not.toBe('');
+    // Should create user with fakeUser data
+    expect(user).toMatchObject(fakeUser);
+
+    /* Response */
+    // Should have status 201
+    expect(status).toBe(StatusCodes.CREATED);
+    // Should respond with the user created
+    expect(responseUser).toMatchObject(normalize(user));
   });
 
-  test('It should be able to show a user', async () => {
+  test('It should be able to display a user', async () => {
+    const user = await User.findOne({ order: sequelize.random() });
+    const { status, body: responseUser } = await request(app).get(`/users/${user.id}`);
 
+    /* Response */
+    // Should have status 200
+    expect(status).toBe(StatusCodes.OK);
+    // Should respond with the user specified
+    expect(responseUser).toMatchObject(normalize(user));
   });
 
   test('It should be able to update a user', async () => {
+    const fakeUser = buildFakeUser();
+    const user = await User.findOne({ order: sequelize.random() });
+    const { status, body: responseUser } = await request(app).put(`/users/${user.id}`).send(fakeUser);
+    const updatedUser = await User.findByPk(user.id);
 
+    /* Database */
+    // Should update user with fakeUser data after PUT request
+    expect(updatedUser).toMatchObject(fakeUser);
+
+    /* Response */
+    // Should have status 200
+    expect(status).toBe(StatusCodes.OK);
+    // Should respond with the user updated
+    expect(responseUser).toMatchObject(normalize(updatedUser));
   });
 
   test('It should be able to delete a user', async () => {
+    const user = await User.findOne({ order: sequelize.random() });
+    const { status, body: responseUser } = await request(app).delete(`/users/${user.id}`);
+    const deletedUser = await User.findByPk(user.id);
 
+    /* Database */
+    // Should delete user in database after DELETE request
+    expect(deletedUser).toBeNull();
+
+    /* Response */
+    // Should have status 200
+    expect(status).toBe(StatusCodes.OK);
+    // Should respond with the user deleted
+    expect(responseUser).toMatchObject(normalize(user));
   });
 });
