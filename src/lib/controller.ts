daf041038643bec5b98b284037f8536418f4dba6
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-05 10:53:36 +0900

    feat: type check internalServerError & give friendly feedback

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -2,6 +2,15 @@ import type { RequestHandler } from 'express';
 import { ReasonPhrases } from 'http-status-codes';
 import type * as JSONAPI from 'jsonapi-typescript';
 
+const isSerializable = (value: any) => {
+  try {
+    JSON.parse(JSON.stringify(value));
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
 export interface Controller {
   index?: RequestHandler;
   create?: RequestHandler;
@@ -19,11 +28,23 @@ export const resourceNotFound = () => {
   return errorResponse;
 };
 
-export const internalServerError = (error: any) => {
+export const internalServerError = (error: unknown) => {
+  console.error(error);
+
+  let detail = `The server encountered an internal error or misconfiguration and was unable to complete your request.
+Please contact the server administrator and inform them of the time the error occurred and anything you might have done that may have caused the error.
+More information about this error may be available in the server error log.`;
+
+  if (error instanceof Error) {
+    detail = error.toString();
+  } else if (isSerializable(error)) {
+    detail = JSON.stringify(error);
+  }
+
   const errorResponse: JSONAPI.ErrorObject = {
     title: 'Internal Server Error',
     status: ReasonPhrases.INTERNAL_SERVER_ERROR,
-    meta: error,
+    detail,
   };
 
   return errorResponse;
