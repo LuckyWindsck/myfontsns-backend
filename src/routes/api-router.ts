6173f938b63fcbb2e96f595bbb913d3b3c8eb3af
LuckyWind_sck <luckywindsck@gmail.com>
2021-07-09 18:30:40 +0900

    feat: stop authentification temporarily
    For end-of-term presentation.

--- src/routes/api-router.ts
+++ src/routes/api-router.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 
 import { JSONAPIResponseInterceptor } from '../util/json-api';
 
-import SessionRouter from './session-router';
+// Note: stop authentification temporarily
+// import SessionRouter from './session-router';
 import UserRouter from './user-router';
 
 const APIRouter = Router();
 
 APIRouter.use(JSONAPIResponseInterceptor);
-APIRouter.use('/sessions', SessionRouter);
+// APIRouter.use('/sessions', SessionRouter);
 APIRouter.use('/users', UserRouter);
 
 export default APIRouter;
--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 
 import UserController from '../controllers/user-controller';
-import { requireAuthentification } from '../util/express/middleware';
+// Note: stop authentification temporarily
+// import { requireAuthentification } from '../util/express/middleware';
 
 const UserRouter = Router();
 
 // TODO: limit current user permission
 
-UserRouter.route('/').get(requireAuthentification);
+// UserRouter.route('/').get(requireAuthentification);
 if (UserController.index) UserRouter.route('/').get(UserController.index);
 
 if (UserController.create) UserRouter.route('/').post(UserController.create);
 
-UserRouter.route('/:id').all(requireAuthentification);
+// UserRouter.route('/:id').all(requireAuthentification);
 if (UserController.show) UserRouter.route('/:id').get(UserController.show);
 if (UserController.update) UserRouter.route('/:id').put(UserController.update).patch(UserController.update);
 if (UserController.destroy) UserRouter.route('/:id').delete(UserController.destroy);
